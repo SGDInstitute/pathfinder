de323aeb3fa3daf4e0c6a122239f1eb75f21161e
Andy Newhouse <hi@andymnewhouse.me>
2023-08-29 20:04:14 -0500

    Add pronouns

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'pronouns' => match (rand(1, 3)) {
+                1 => 'he/him',
+                2 => 'she/her',
+                3 => 'they/them',
+            },
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- resources/views/components/print-row.blade.php
+++ resources/views/components/print-row.blade.php
@@ -1,9 +1,10 @@
-<div @class(['rounded-md p-4 flex space-x-4', 'bg-gray-100'=> $odd])>
+<div @class(['rounded-md p-4 flex items-center space-x-4', 'bg-gray-100'=> $odd])>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green-600">
         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
 
     <div>
-        {{ $badge->name }}
+        <h2>{{ $badge->name }}</h2>
+        <p>{{ $badge->pronouns }}</p>
     </div>
 </div>
